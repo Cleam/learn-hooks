27b09d472d4e959a74850579c0eef141bb55f957
lizhigao <lizhigao@021.com>
2020-02-24 15:47:28 +0800

    useState基本使用

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // 声明一个新的叫做 “count” 的 state 变量
   const [count, setCount] = useState(0);
   return (
-    <div className='App'>
+    <div className="App">
       <p>你点击了{count}次。</p>
       <button onClick={() => setCount(count + 1)}>点我</button>
     </div>
