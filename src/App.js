f070d9335ee29d5c4206d3f696307c049b0fd22e
lizhigao <lizhigao@021.com>
2020-02-24 18:15:27 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Transition } from 'react-transition-group';
+import { Container, Button, Alert } from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
@@ -14,34 +16,60 @@ function App() {
   // );
 
   // demo2 - react动画库（react-transition-group）学习
-  const duration = 300;
-  const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0
-  };
-  const transitionStyles = {
-    entering: { opacity: 1 },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 }
-  };
-  const [inProp, setInProp] = useState(false);
+  // const duration = 300;
+  // const defaultStyle = {
+  //   transition: `opacity ${duration}ms ease-in-out`,
+  //   opacity: 0
+  // };
+  // const transitionStyles = {
+  //   entering: { opacity: 1 },
+  //   entered: { opacity: 1 },
+  //   exiting: { opacity: 0 },
+  //   exited: { opacity: 0 }
+  // };
+  // const [inProp, setInProp] = useState(false);
+  // return (
+  //   <div>
+  //     <Transition in={inProp} timeout={500}>
+  //       {state => (
+  //         <div
+  //           style={{
+  //             ...defaultStyle,
+  //             ...transitionStyles[state]
+  //           }}
+  //         >
+  //           I'm a fade Transition!
+  //         </div>
+  //       )}
+  //     </Transition>
+  //     <button onClick={() => setInProp(true)}>Click to Enter</button>
+  //   </div>
+  // );
+
+  const [showButton, setShowButton] = useState(true);
+  const [showMessage, setShowMessage] = useState(false);
   return (
-    <div>
-      <Transition in={inProp} timeout={500}>
-        {state => (
-          <div
-            style={{
-              ...defaultStyle,
-              ...transitionStyles[state]
-            }}
-          >
-            I'm a fade Transition!
-          </div>
-        )}
-      </Transition>
-      <button onClick={() => setInProp(true)}>Click to Enter</button>
-    </div>
+    <Container style={{ paddingTop: '2rem' }}>
+      {showButton && (
+        <Button onClick={() => setShowMessage(true)} size='lg'>
+          Show Message
+        </Button>
+      )}
+      <CSSTransition
+        in={showMessage}
+        timeout={300}
+        classNames='alert'
+        unmountOnExit
+        onEnter={() => setShowButton(false)}
+        onExited={() => setShowButton(true)}
+      >
+        <Alert variant='primary' dismissible onClose={() => setShowMessage(false)}>
+          <Alert.Heading>Animated alert message</Alert.Heading>
+          <p>This alert message is being transitioned in and out of the DOM.</p>
+          <Button onClick={() => setShowMessage(false)}>Close</Button>
+        </Alert>
+      </CSSTransition>
+    </Container>
   );
 }
 
