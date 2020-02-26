c48b29ee61b805db38eed7a090c5dd14eaaad694
lizhigao <lizhigao@021.com>
2020-02-26 17:25:07 +0800

    useState with object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Container, Button, Alert } from 'react-bootstrap';
-import { CSSTransition } from 'react-transition-group';
+import React, { useState, useEffect } from 'react';
+// import { Container, Button, Alert } from 'react-bootstrap';
+// import { CSSTransition } from 'react-transition-group';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -46,30 +46,69 @@ function App() {
   //   </div>
   // );
 
-  const [showButton, setShowButton] = useState(true);
-  const [showMessage, setShowMessage] = useState(false);
+  // demo3 - 动画学习
+  // const [showButton, setShowButton] = useState(true);
+  // const [showMessage, setShowMessage] = useState(false);
+  // return (
+  //   <Container style={{ paddingTop: '2rem' }}>
+  //     {showButton && (
+  //       <Button onClick={() => setShowMessage(true)} size='lg'>
+  //         Show Message
+  //       </Button>
+  //     )}
+  //     <CSSTransition
+  //       in={showMessage}
+  //       timeout={300}
+  //       classNames='alert'
+  //       unmountOnExit
+  //       onEnter={() => setShowButton(false)}
+  //       onExited={() => setShowButton(true)}
+  //     >
+  //       <Alert variant='primary' dismissible onClose={() => setShowMessage(false)}>
+  //         <Alert.Heading>Animated alert message</Alert.Heading>
+  //         <p>This alert message is being transitioned in and out of the DOM.</p>
+  //         <Button onClick={() => setShowMessage(false)}>Close</Button>
+  //       </Alert>
+  //     </CSSTransition>
+  //   </Container>
+  // );
+
+  // demo4 hooks状态为对象情况
+  // 声明一个新的叫做 “count” 的 state 变量
+  const [obj, setObj] = useState({});
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  useEffect(() => {
+    let o = {};
+    for (let i = 0; i < arr.length; i++) {
+      o[i + 1] = true;
+    }
+    setObj(o);
+  }, [arr.length]);
+
+  const handleClick = () => {
+    const randomNum = Math.floor(Math.random() * 10);
+    setObj({
+      ...obj,
+      [randomNum]: false
+    });
+  };
+
+  const listEl = arr.map(i => {
+    return (
+      obj[i] && (
+        <div key={i} className='item'>
+          li - {i}
+        </div>
+      )
+    );
+  });
+
   return (
-    <Container style={{ paddingTop: '2rem' }}>
-      {showButton && (
-        <Button onClick={() => setShowMessage(true)} size='lg'>
-          Show Message
-        </Button>
-      )}
-      <CSSTransition
-        in={showMessage}
-        timeout={300}
-        classNames='alert'
-        unmountOnExit
-        onEnter={() => setShowButton(false)}
-        onExited={() => setShowButton(true)}
-      >
-        <Alert variant='primary' dismissible onClose={() => setShowMessage(false)}>
-          <Alert.Heading>Animated alert message</Alert.Heading>
-          <p>This alert message is being transitioned in and out of the DOM.</p>
-          <Button onClick={() => setShowMessage(false)}>Close</Button>
-        </Alert>
-      </CSSTransition>
-    </Container>
+    <div className='App'>
+      <div className='list'>{listEl}</div>
+      <button onClick={handleClick}>点我</button>
+    </div>
   );
 }
 
